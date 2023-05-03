8bda5a3fe817b4583c9ed06b4a5d4c112ec6a5d4
Zain <zat938@gmail.com>
2023-05-04 00:14:41 +0500

    if appllied, this commit do a cleanup of folders and move into gitignore file

--- dist/jest.config.dev.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-module.exports = {
-  testEnvironment: "node",
-  test: "cross-env NODE_ENV=test jest --testTimeout=10000",
-  pretest: "cross-env NODE_ENV=test npm run db:reset",
-  "db:create:test": "cross-env NODE_ENV=test npx sequelize-cli db:create"
-};
--- dist/server.dev.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var app = require('./index');
-
-var PORT = process.env.PORT || 8000;
-app.listen(PORT, function () {
-  console.log("server is running on PORT ".concat(PORT));
-});
