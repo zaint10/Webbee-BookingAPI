6c1c7454cd472367657582640e6a2fc28b528224
Zain <zat938@gmail.com>
2023-05-05 04:44:21 +0500

    if applied, this commit adds some more test cases

--- tests/scheduling.test.js
+++ tests/scheduling.test.js
@@ -38,6 +38,7 @@ describe("API tests", () => {
 
   describe("POST /api/scheduling/book-appointment", () => {
     let service, schedule, user;
+    let date = moment().format("YYYY-MM-DD");
 
     beforeAll(async () => {
       // Create a service, schedule and user
@@ -47,24 +48,30 @@ describe("API tests", () => {
         max_clients_per_slot: 1,
         cleanup_duration: 10,
       });
-
     });
 
     beforeEach(async () => {
       // Create a schedule for each test case
       schedule = await db.Schedule.create({
         service_id: service.id,
-        day_of_week: moment().day(),
+        day_of_week: moment(date).day(),
         start_time: "10:00",
         end_time: "18:00",
         break_start_time: "12:00:00",
         break_end_time: "13:00:00",
       });
+
+      user = await db.User.create({
+        first_name: "Test",
+        last_name: "User",
+        email: "test@example.com",
+      });
     });
-  
+
     afterEach(async () => {
       // Clean up the test data after each test case
       await db.Appointment.destroy({ where: {} });
+      await db.User.destroy({ where: {} });
       await db.Schedule.destroy({ where: {} });
       await db.User.destroy({ where: {} });
     });
@@ -73,13 +80,12 @@ describe("API tests", () => {
       // Clean up the test data after all tests are done
       await db.Service.destroy({ where: {} });
     });
-  
 
     it("should book an appointment with valid request body", async () => {
       const requestBody = {
         serviceId: service.id,
         scheduleId: schedule.id,
-        appointmentDate: "2023-05-05",
+        appointmentDate: date,
         startTime: "10:00",
         endTime: "10:40",
         users: [
@@ -93,8 +99,8 @@ describe("API tests", () => {
       const response = await request(app)
         .post("/api/scheduling/book-appointment")
         .send(requestBody);
-        console.log('requestBody', requestBody)
-      console.log('response', response.body)
+      console.log("requestBody", requestBody);
+      console.log("response", response.body);
       expect(response.statusCode).toBe(201);
       expect(response.body.appointments).toBeDefined();
       expect(response.body.appointments.length).toBeGreaterThan(0);
@@ -112,21 +118,12 @@ describe("API tests", () => {
     });
 
     it("should return an error message when requested slot is outside of working hours", async () => {
-      schedule = await db.Schedule.create({
-        service_id: service.id,
-        day_of_week: moment().day(),
-        start_time: "10:00",
-        end_time: "18:00",
-        break_start_time: "12:00:00",
-        break_end_time: "13:00:00",
-      });
-
       const response = await request(app)
         .post("/api/scheduling/book-appointment")
         .send({
           serviceId: service.id,
           scheduleId: schedule.id,
-          appointmentDate: moment().format("YYYY-MM-DD"),
+          appointmentDate: date,
           startTime: "09:00",
           endTime: "09:30",
           users: [
@@ -141,59 +138,87 @@ describe("API tests", () => {
       );
     });
 
-    it('returns an error when the requested slot is fully booked', async () => {
-      schedule = await db.Schedule.create({
-        service_id: service.id,
-        day_of_week: moment().day(),
-        start_time: "09:00:00",
-        end_time: "17:00:00",
-        is_off: false,
-      });
-
-      user = await db.User.create({
-        first_name: "Test",
-        last_name: "User",
-        email: "test@example.com",
-      });
-
+    it("returns an error when the requested slot is fully booked", async () => {
       // Create an appointment to fully book the available slot
       await db.Appointment.create({
         user_id: user.id,
         service_id: service.id,
         schedule_id: schedule.id,
         appointment_date: moment().format("YYYY-MM-DD"),
-        start_time: "09:00:00",
-        end_time: "09:30:00",
+        start_time: "10:00:00",
+        end_time: "10:40:00",
       });
-      
-      const date = moment().format('YYYY-MM-DD');
-      const startTime = '09:00';
-      const endTime = '09:30';
 
-      console.log({
-        serviceId: service.id,
-        scheduleId: schedule.id,
-        appointmentDate: date,
-        startTime: startTime,
-        endTime: endTime,
-        users: [{  first_name: "JohnZ", last_name: "Test", email: "john@example.com"  }],
-      }, 'ZAINN')
-  
+      const startTime = "10:00";
+      const endTime = "10:40";
+
       const response = await request(app)
-        .post('/api/scheduling/book-appointment')
+        .post("/api/scheduling/book-appointment")
         .send({
           serviceId: service.id,
           scheduleId: schedule.id,
           appointmentDate: date,
           startTime: startTime,
           endTime: endTime,
-          users: [{  first_name: "JohnZ", last_name: "Test", email: "john@example.com"  }],
+          users: [
+            {
+              first_name: "JohnZ",
+              last_name: "Test",
+              email: "john@example.com",
+            },
+          ],
         });
-  
+
       expect(response.status).toEqual(404);
+      expect(response.body.message).toBe("Requested slot is fully booked.");
+    });
+
+    it("should return an error when the requested slot is before the current date/time", async () => {
+      const yesterday = moment().subtract(1, "days").format("YYYY-MM-DD");
+      const response = await request(app)
+        .post("/api/scheduling/book-appointment")
+        .send({
+          serviceId: service.id,
+          scheduleId: schedule.id,
+          appointmentDate: yesterday,
+          startTime: "10:00",
+          endTime: "10:40",
+          users: [
+            {
+              first_name: "Jane",
+              last_name: "Doe",
+              email: "janedoe@example.com",
+            },
+          ],
+        });
+
+      expect(response.statusCode).toBe(404);
       expect(response.body.message).toBe(
-        "Requested slot is fully booked."
+        "Invalid appointment date. You can only book up to 7 days in advance."
       );
+    });
+
+    it("should return an error when the requested slot is too far in the future", async () => {
+      const futureDate = moment().add(31, "days").format("YYYY-MM-DD");
+      const response = await request(app)
+        .post("/api/scheduling/book-appointment")
+        .send({
+          serviceId: service.id,
+          scheduleId: schedule.id,
+          appointmentDate: futureDate,
+          startTime: "10:00",
+          endTime: "10:40",
+          users: [
+            {
+              first_name: "Jane",
+              last_name: "Doe",
+              email: "janedoe@example.com",
+            },
+          ],
+        });
+
+      expect(response.statusCode).toBe(404);
+      expect(response.body.message).toBe("Invalid appointment date. You can only book up to 7 days in advance.");
     });
   });
 });
